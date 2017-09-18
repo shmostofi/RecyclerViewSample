79cb873bce03ccf6155da77239762583bccac222
shokooh <shokouh.mostofi@gmail.com>
2017-09-17 20:21:48 -0400

    refactored by adding View package

--- RecyclerViewSampleApp/app/src/main/java/com/example/shokooh/recyclerviewsampleapp/View/ListActivity.java
+++ RecyclerViewSampleApp/app/src/main/java/com/example/shokooh/recyclerviewsampleapp/View/ListActivity.java
@@ -1,7 +1,9 @@
-package com.example.shokooh.recyclerviewsampleapp;
+package com.example.shokooh.recyclerviewsampleapp.View;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.shokooh.recyclerviewsampleapp.R;
 
 public class ListActivity extends AppCompatActivity {
 
